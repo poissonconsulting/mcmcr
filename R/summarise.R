2e1c232885b283b2fe768ad230c37697fb731789
joethorley <joe@poissonconsulting.ca>
2017-02-20 14:56:48 -0800

    summarise drop .fun arg

--- R/summarise.R
+++ R/summarise.R
@@ -1,18 +1,18 @@
-summarise_vector_index <- function(i, vector, idx, fun) {
-  fun(vector[idx == i])
+summarise_vector_index <- function(i, vector, idx) {
+  sum(vector[idx == i])
 }
 
-summarise_vector <- function(x, idx, fun) {
+summarise_vector <- function(x, idx) {
   idxs <- unique(idx)
-  idxs %<>% vapply(FUN = summarise_vector_index, FUN.VALUE = 1, vector = x, idx = idx, fun = fun, USE.NAMES = FALSE)
+  idxs %<>% vapply(FUN = summarise_vector_index, FUN.VALUE = 1, vector = x, idx = idx, USE.NAMES = FALSE)
   idxs
 }
 
-summarise_mcmcarray <- function(x, idx, fun) {
+summarise_mcmcarray <- function(x, idx) {
   stopifnot(is.mcmcarray(x))
   stopifnot(ndims(x) == 3)
 
-  x %<>% plyr::aaply(.margins = c(1,2), .fun = summarise_vector, idx = idx, fun = fun, .drop = FALSE)
+  x %<>% plyr::aaply(.margins = c(1,2), .fun = summarise_vector, idx = idx, .drop = FALSE)
   class(x) <- "mcmcarray"
   x
 }
@@ -21,12 +21,11 @@ summarise_mcmcarray <- function(x, idx, fun) {
 #'
 #' @inheritParams dplyr::summarise_
 #' @param .x An mcmcr_data object
-#' @param .fun A function to summarise mcmc samples by
 #' @export
-summarise_.mcmcr_data <- function(.x, ..., .dots, .fun = mean){
+summarise_.mcmcr_data <- function(.x, ..., .dots){
   if (is.null(dplyr::groups(.x$data))) error("mcmcr_data must be grouped to summarize")
   IDX <- dplyr::group_indices(.x$data)
-  .x$mcmcr[[1]] %<>% summarise_mcmcarray(IDX, fun = .fun)
+  .x$mcmcr[[1]] %<>% summarise_mcmcarray(IDX)
   .x$data %<>% dplyr::summarise_(..., .dots = .dots)
   .x
 }
