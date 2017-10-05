06bf567d86f67d844636949925f68ebcebea77cf
Joe Thorley <joe@poissonconsulting.ca>
2017-10-05 09:58:04 -0700

    estimates as tbl

--- R/estimates.R
+++ R/estimates.R
@@ -4,18 +4,50 @@
 #'
 #' @param object The object.
 #' @param ...  Not used.
-#' @return The estimates as a named list.
 #' @export
 estimates <- function(object, ...) {
   UseMethod("estimates")
 }
 
+
 #' @export
-estimates.mcmcarray <- function(object, fun = stats::median, ...) {
-    apply(object, 3:ndims(object), FUN = fun, ...)
+estimates.mcmcarray <- function(object, fun = stats::median, as_list = TRUE, ...) {
+  check_flag(as_list)
+
+  ndims <- ndims(object)
+  object %<>% apply(3:ndims, FUN = fun, ...)
+
+  if (as_list) return(object)
+
+  object %<>% reshape2::melt()
+
+  if (nrow(object) == 1) {
+    object$term <- ""
+  } else {
+    values <- object["value"]
+    if (ncol(object) == 1) object$Var1 <- 1:nrow(object)
+
+    object %<>%
+      dplyr::select_(~-value)
+
+    object %<>%
+      tidyr::unite_("term", from = colnames(.), sep = ",") %>%
+      dplyr::mutate_(term = ~paste0("[", term, "]")) %>%
+      dplyr::bind_cols(values)
+  }
+  object$term %<>% as.term()
+  object <- object[c("term", "value")]
+  colnames(object) <- c("term", "estimate")
+  dplyr::as.tbl(object)
 }
 
 #' @export
-estimates.mcmcr <- function(object, fun = stats::median, ...) {
-  llply(object, estimates, fun, ...)
+estimates.mcmcr <- function(object, fun = stats::median, as_list = TRUE, ...) {
+  object %<>% llply(estimates, fun = fun, as_list = as_list, ...)
+  if (as_list) return(object)
+
+  suppressWarnings(object %<>% dplyr::bind_rows(.id = "id"))
+  object %<>%  tidyr::unite_("term", from = c("id", "term"), sep = "")
+  object$term %<>% as.term()
+  object
 }
--- tests/testthat/test-estimates.R
+++ tests/testthat/test-estimates.R
@@ -5,3 +5,10 @@ test_that("estimates", {
   expect_equal(estimates(mcmcr[[3]]), c(0.8265352))
   expect_identical(estimates(mcmcr)$alpha, estimates(mcmcr[[1]]))
 })
+
+test_that("estimates not as list", {
+  expect_equal(estimates(mcmcr[[1]], as_list = FALSE), dplyr::data_frame(term = as.term(c("[1]", "[2]")), estimate = estimates(mcmcr[[1]])))
+  expect_equal(estimates(mcmcr[[2]], as_list = FALSE), dplyr::data_frame(term = as.term(c("[1,1]", "[2,1]", "[1,2]", "[2,2]")), estimate = as.vector(estimates(mcmcr[[2]]))))
+  expect_equivalent(estimates(mcmcr[[3]], as_list = FALSE), dplyr::data_frame(term = as.term(""), estimate = estimates(mcmcr[[3]])))
+  expect_identical(sort(estimates(mcmcr, as_list = FALSE)$term), sort(as.term(c("alpha[1]", "alpha[2]", "beta[1,1]", "beta[2,1]", "beta[1,2]", "beta[2,2]", "sigma"))))
+})
