6bcd70dcc825c5490f90121938ee8261933e98f7
Joe Thorley <joe@poissonconsulting.ca>
2018-02-20 14:00:28 -0800

    mcmc_map

--- tests/testthat/test-mcmc-map.R
+++ tests/testthat/test-mcmc-map.R
@@ -1,12 +1,10 @@
 context("mcmc-map")
 
 test_that("mcmc_map", {
-
-  expect_identical(mcmc_map(mcmcr[[1]], identity), mcmcr[[1]])
-  expect_identical(mcmc_map(mcmcr[[1]], function(x) x * 10), mcmcr[[1]] * 10)
-  expect_error(mcmc_map(mcmcr[[1]], function(x) c(x,x)))
-  expect_identical(sum(mcmc_map(mcmcr[[1]], multiply_by, 2)), sum(mcmcr[[1]]) * 2)
-
-  expect_is(mcmc_map(mcmcr, function(x) x * 5), "mcmcr")
-  expect_identical(mcmc_map(mcmcr, function(x) x * 5)[[2]], mcmcr[[2]] * 5)
+  expect_identical(mcmc_map(mcmcr::mcmcr_example[[1]], identity), mcmcr::mcmcr_example[[1]])
+  expect_identical(mcmc_map(mcmcr::mcmcr_example[[1]], function(x) x * 10), mcmcr::mcmcr_example[[1]] * 10)
+  expect_error(mcmc_map(mcmcr::mcmcr_example[[1]], function(x) c(x,x)))
+  expect_identical(sum(mcmc_map(mcmcr::mcmcr_example[[1]], function(x) (x * 2))), sum(mcmcr::mcmcr_example[[1]]) * 2)
+  expect_is(mcmc_map(mcmcr::mcmcr_example, function(x) x * 5), "mcmcr")
+  expect_identical(mcmc_map(mcmcr::mcmcr_example, function(x) x * 5)[[2]], mcmcr::mcmcr_example[[2]] * 5)
 })
