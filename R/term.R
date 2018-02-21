8aeb3a98a88200f6e2abf1ebd1667cad2588c5e0
Joe Thorley <joe@poissonconsulting.ca>
2018-02-20 16:42:49 -0800

    remove purrr dependency

--- R/term.R
+++ R/term.R
@@ -75,7 +75,7 @@ rep.term <- function(x, times, ...) {
   if (identical(length(e1), 1L)) e1 <- rep(e1, length(e2))
   if (identical(length(e2), 1L)) e2 <- rep(e2, length(e1))
 
-  purrr::map2_lgl(e1, e2, greater_than_term)
+  mapply(e1, e2, FUN = greater_than_term, USE.NAMES = FALSE)
 }
 
 #' @export
