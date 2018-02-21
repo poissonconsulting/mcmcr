7725b0ae21a0d55ada4027b99e105a39bcb06ae3
Joe Thorley <joe@poissonconsulting.ca>
2018-02-21 08:14:40 -0800

    moved derive to mcmcderive package

--- R/internal.R
+++ R/internal.R
@@ -5,28 +5,6 @@ set_class <- function(x, class) {
   x
 }
 
-derive_sample <- function(i, object, expr, values, monitor) {
-  object <- subset(object, iterations = i)
-  object <- estimates(object)
-  object <- c(object, values)
-  object <- within(object, eval(expr))
-  object <- object[monitor]
-  object <- lapply(object, function(x) { dim(x) <- c(1L, 1L, dims(x)); class(x) <- "mcmcarray"; x})
-
-  class(object) <- "mcmcr"
-
-  object
-}
-
-derive_chain <- function(i, object, expr, values, monitor) {
-  object <- subset(object, chains = i)
-
-  object <- lapply(1:niters(object), FUN = derive_sample, object = object,
-                  expr = expr, values = values, monitor = monitor)
-  object <- Reduce(bind_iterations, object)
-  object
-}
-
 dims_terms <- function(x) {
   x <- sub("^(\\w+)(.*)", "\\2", x)
   x <- sub("^(\\[)(.*)(\\])$", "\\2", x)
