2ced5d7bdedb69ba4d379d760a306e8775d08224
Joe Thorley <joe@poissonconsulting.ca>
2018-04-05 11:53:57 -0700

    document all anyNA

--- R/anyNA.R
+++ R/anyNA.R
@@ -1,27 +1,38 @@
 #' Any Missing Values
 #'
-#' Returns a flag indicating whether there are any missing values.
+#' Test whether there are any missing values.
 #'
 #' @param x The object to test.
 #' @param recursive Unused.
+#' @return A flag indicating whether there are any missing values.
+#' @seealso \code{base::\link[base]{anyNA}}
 #' @examples
 #' anyNA(mcmcr_example)
 #' anyNA(mcmcr_example$beta)
 #'
+#' @name anyNA
+NULL
+
+#' @rdname anyNA
 #' @export
 anyNA.mcarray <- function(x, recursive = FALSE) anyNA(unclass(x))
 
+#' @rdname anyNA
 #' @export
 anyNA.mcmc <- function(x, recursive = FALSE) anyNA(unclass(x))
 
+#' @rdname anyNA
 #' @export
 anyNA.mcmc.list <- function(x, recursive = FALSE) any(vapply(x, anyNA, TRUE))
 
+#' @rdname anyNA
 #' @export
 anyNA.mcmcarray <- function(x, recursive = FALSE) anyNA(unclass(x))
 
+#' @rdname anyNA
 #' @export
 anyNA.mcmcr <- function(x, recursive = FALSE) any(vapply(x, anyNA, TRUE))
 
+#' @rdname anyNA
 #' @export
 anyNA.mcmcrs <- function(x, recursive = FALSE) any(vapply(x, anyNA, TRUE))
