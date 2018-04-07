c6129b9a8f9d9414c1587cd67c9fe042b1551abc
Joe Thorley <joe@poissonconsulting.ca>
2018-04-07 14:27:17 -0700

    rhat docs

--- R/rhat.R
+++ R/rhat.R
@@ -1,12 +1,17 @@
-#' Rhat
+#' R-hat
 #'
-#' Calculates uncorrected, untransformed, univariate
+#' Calculates the uncorrected, untransformed, univariate
 #' split R-hat (potential scale reduction factor) values.
+#'
+#' @param x An MCMC object.
+#' @param ... Unused.
+#' @param by A string indicating whether to determine by
+#' "term", "parameter" or "all".
+#' @param as_df A flag indicating whether to return the values as a
+#' tibble versus a named list.
+#' @return The rhat value(s).
 #' @references
 #' Gelman, A., and Rubin, D.B. 1992. Inference from Iterative Simulation Using Multiple Sequences. Statistical Science 7(4): 457–472.
-#' @param x An mcmc object.
-#' @param ... Unused.
-#' @return The rhat value(s).
 #' @export
 #' @examples
 #' rhat(mcmcr_example)
@@ -17,18 +22,22 @@ rhat <- function(x, ...) {
   UseMethod("rhat")
 }
 
+#' @describeIn rhat R-hat for an mcarray object
 #' @export
 rhat.mcarray <- function(x, by = "all", as_df = FALSE, ...)
   rhat(as.mcmcarray(x), by = by, as_df = as_df)
 
+#' @describeIn rhat R-hat for an mcmc object
 #' @export
 rhat.mcmc <- function(x, by = "all", as_df = FALSE, ...)
   rhat(as.mcmcr(x), by = by, as_df = as_df)
 
+#' @describeIn rhat R-hat for an mcmc.list object
 #' @export
 rhat.mcmc.list <- function(x, by = "all", as_df = FALSE, ...)
   rhat(as.mcmcr(x), by = by, as_df = as_df)
 
+#' @describeIn rhat R-hat for an mcmcarray object
 #' @export
 rhat.mcmcarray <- function(x, by = "all", as_df = FALSE, ...) {
   check_vector(by, c("all", "parameter", "term"), length = 1)
@@ -58,6 +67,7 @@ rhat.mcmcarray <- function(x, by = "all", as_df = FALSE, ...) {
   x
 }
 
+#' @describeIn rhat R-hat for an mcmcr object
 #' @export
 rhat.mcmcr <- function(x, by = "all", as_df = FALSE, ...) {
   parameters <- parameters(x)
@@ -73,6 +83,7 @@ rhat.mcmcr <- function(x, by = "all", as_df = FALSE, ...) {
   x
 }
 
+#' @describeIn rhat R-hat for an mcmcrs object
 #' @export
 rhat.mcmcrs <- function(x, by = "all", as_df = FALSE, ...) {
   lapply(x, rhat, by = by, as_df = as_df)
