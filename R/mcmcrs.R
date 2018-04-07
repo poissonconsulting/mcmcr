26d7b11040ae412b50e8a94b61b765f5b1748773
Joe Thorley <joe@poissonconsulting.ca>
2018-04-06 17:27:44 -0700

    docs nchains

--- R/mcmcrs.R
+++ R/mcmcrs.R
@@ -1,7 +1,8 @@
-#' MCMCRs
+#' Create mcmcrs
+#'
+#' Creates an \code{\link{mcmcrs-object}} from multiple \code{link{mcmcr-object}}s.
 #'
 #' @param ... Objects of class mcmcr.
-#'
 #' @return An object of class mcmcrs
 #' @export
 #' @examples
--- R/nchains.R
+++ R/nchains.R
@@ -1,8 +1,8 @@
 #' Number of MCMC chains
 #'
-#' @param x The object
-#' @param ... Unused.
-#' @return A count indicating the number of MCMC chains in object.
+#' @param x An MCMC object
+#' @param ... Unused
+#' @return A count indicating the number of MCMC chains
 #' @export
 #' @examples
 #' nchains(mcmcr_example)
@@ -12,20 +12,26 @@ nchains <- function(x, ...) {
 
 nchains.matrix <- function(x, ...) nrow(x)
 
+#' @describeIn nchains Number of MCMC chains for an mcarray object
 #' @export
 nchains.mcarray <- function(x, ...) unname(dim(x)[ndims(x)])
 
+#' @describeIn nchains Number of MCMC chains for an mcmc object
 #' @export
 nchains.mcmc <- function(x, ...) 1L
 
+#' @describeIn nchains Number of MCMC chains for an mcmc.list object
 #' @export
 nchains.mcmc.list <- function(x, ...) length(x)
 
+#' @describeIn nchains Number of MCMC chains for an mcmcarray object
 #' @export
 nchains.mcmcarray <- function(x, ...) dim(x)[1]
 
+#' @describeIn nchains Number of MCMC chains for an mcmcr object
 #' @export
 nchains.mcmcr <- function(x, ...) nchains(x[[1]])
 
+#' @describeIn nchains Number of MCMC chains for an mcmcrs object
 #' @export
 nchains.mcmcrs <- function(x, ...) nchains(x[[1]])
