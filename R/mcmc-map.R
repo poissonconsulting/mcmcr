1e23e0f6460681bc9311a2dc071207e0be45c830
Joe Thorley <joe@poissonconsulting.ca>
2018-04-06 17:16:06 -0700

    mcmc_map docs

--- R/mcmc-map.R
+++ R/mcmc-map.R
@@ -1,16 +1,19 @@
-#' MCMC map
+#' MCMC Map
 #'
-#' Map mcmc values
+#' Adjust the sample values of an MCMC object using a function.
 #'
-#' @param .x The mcmc object.
+#' @param .x An MCMC object
 #' @param .f The function to use
 #' @param ... Additional arguments passed to .f.
-#' @return The mapped mcmc object.
+#' @return The updated MCMC object.
 #' @export
+#' @examples
+#' mcmc_map(mcmcr_example$beta, exp)
 mcmc_map <- function(.x, .f, ...) {
   UseMethod("mcmc_map")
 }
 
+#' @describeIn mcmc_map Adjust the sample values of an MCMC object
 #' @export
 mcmc_map.default <- function(.x, .f, ...) {
   x <- .f(.x, ...)
@@ -18,21 +21,23 @@ mcmc_map.default <- function(.x, .f, ...) {
   x
 }
 
+#' @describeIn mcmc_map Adjust the sample values of an mcmc.list object
 #' @export
 mcmc_map.mcmc.list <- function(.x, .f, ...) {
   x <- lapply(.x, mcmc_map, .f = .f, ...)
   coda::as.mcmc.list(x)
 }
 
+#' @describeIn mcmc_map Adjust the sample values of an mcmcr object
 #' @export
 mcmc_map.mcmcr <- function(.x, .f, ...) {
   x <- lapply(.x, mcmc_map, .f = .f, ...)
   set_class(x, "mcmcr")
 }
 
+#' @describeIn mcmc_map Adjust the sample values of an mcmcrs object
 #' @export
 mcmc_map.mcmcrs <- function(.x, .f, ...) {
   x <- lapply(.x, mcmc_map, .f = .f, ...)
   set_class(x, "mcmcrs")
 }
-
