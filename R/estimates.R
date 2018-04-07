a6a2292da08f97d1e3161d45756e237b1ca01a57
Joe Thorley <joe@poissonconsulting.ca>
2018-04-06 17:00:24 -0700

    estimates docs

--- R/estimates.R
+++ R/estimates.R
@@ -1,16 +1,21 @@
-#' Get Estimates
+#' Estimates
 #'
-#' Gets the estimates for an object as a named list.
+#' Get the estimates for an MCMC object.
 #'
-#' @param object The object.
-#' @param ...  Not used.
+#' @param object An MCMC object.
+#' @param fun The function to use.
+#' @param as_df A flag indicating whether to return the estimates as a tibble versus a named list.
+#' @param ... Unused.
+#' @return A named list or tibble.
 #' @export
 #' @examples
 #' estimates(mcmcr_example)
+#' estimates(mcmcr_example, as_df = TRUE)
 estimates <- function(object, ...) {
   UseMethod("estimates")
 }
 
+#' @describeIn estimates Estimates for an mcarray object
 #' @export
 estimates.mcarray <- function(object, fun = stats::median, as_df = FALSE, ...) {
   if(!as_df)
@@ -18,6 +23,7 @@ estimates.mcarray <- function(object, fun = stats::median, as_df = FALSE, ...) {
   estimates(coda::as.mcmc.list(object), fun = fun, as_df = TRUE)
 }
 
+#' @describeIn estimates Estimates for an mcmc object
 #' @export
 estimates.mcmc <- function(object, fun = stats::median, as_df = FALSE, ...) {
   if(!as_df)
@@ -29,6 +35,7 @@ estimates.mcmc <- function(object, fun = stats::median, as_df = FALSE, ...) {
   tibble::tibble(term = terms, estimate = object)
 }
 
+#' @describeIn estimates Estimates for an mcmc.list object
 #' @export
 estimates.mcmc.list <- function(object, fun = stats::median, as_df = FALSE, ...) {
   if(!as_df)
@@ -38,6 +45,7 @@ estimates.mcmc.list <- function(object, fun = stats::median, as_df = FALSE, ...)
   estimates(object, fun = fun, as_df = TRUE)
 }
 
+#' @describeIn estimates Estimates for an mcmcarray object
 #' @export
 estimates.mcmcarray <- function(object, fun = stats::median, as_df = FALSE, ...) {
   check_function(fun)
@@ -50,6 +58,7 @@ estimates.mcmcarray <- function(object, fun = stats::median, as_df = FALSE, ...)
   apply(object, 3:ndims(object), FUN = .estimates, fun = fun)
 }
 
+#' @describeIn estimates Estimates for an mcmcr object
 #' @export
 estimates.mcmcr <- function(object, fun = stats::median, as_df = FALSE, ...) {
   if(as_df)
