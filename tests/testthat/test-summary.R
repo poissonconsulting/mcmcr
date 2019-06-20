139099aab0bcc2d423b858c31e014902fd31d437
joethorley <joe@poissonconsulting.ca>
2019-06-20 08:09:23 -0700

    test summary

---
+++ tests/testthat/test-summary.R
@@ -0,0 +1,11 @@
+context("summary")
+
+test_that("summary.mcmcarray", {
+  expect_equal(summary(mcmcr_example$alpha),
+               structure(list(estimates = c(3.718025, 4.718025), nchains = 2L,
+                              niters = 400L), class = "summary.mcmcarray"))
+})
+
+test_that("summary.mcmcr", {
+  expect_is(summary(mcmcr_example), "summary.mcmcr")
+})
