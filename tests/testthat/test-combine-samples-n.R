87c4cbc6783a6d4249e72a747e89c92aead452c3
joethorley <joe@poissonconsulting.ca>
2019-06-20 08:24:22 -0700

    test combine samples

--- tests/testthat/test-combine-samples-n.R
+++ tests/testthat/test-combine-samples-n.R
@@ -1,12 +1,24 @@
 context("combine_samples_n")
 
 test_that("combine_samples_n.mcmcarray", {
+  expect_identical(combine_samples_n(mcmcr_example$beta), mcmcr_example$beta)
+
+  expect_equal(combine_samples_n(list(mcmcr_example$beta, mcmcr_example$beta, mcmcr_example$beta)), mcmcr_example$beta, check.attributes = FALSE)
+
+  expect_equal(combine_samples_n(list(mcmcr_example$beta, mcmcr_example$beta, mcmcr_example$beta), mcmcr_example$beta), mcmcr_example$beta, check.attributes = FALSE)
+
   expect_equal(combine_samples_n(mcmcr_example$beta, mcmcr_example$beta, mcmcr_example$beta), mcmcr_example$beta, check.attributes = FALSE)
   x <- combine_samples_n(mcmcr_example$beta, mcmcr_example$beta,  mcmcr_example$beta, fun = sum)
   expect_equal(x[1,,,], mcmcr_example$beta[1,,,] * 3)
 })
 
 test_that("combine_samples_n.mcmcr", {
+  expect_identical(combine_samples_n(mcmcr_example), mcmcr_example)
+
+  expect_equal(combine_samples_n(list(mcmcr_example, mcmcr_example, mcmcr_example)), mcmcr_example, check.attributes = FALSE)
+
+  expect_equal(combine_samples_n(list(mcmcr_example, mcmcr_example, mcmcr_example), mcmcr_example), mcmcr_example, check.attributes = FALSE)
+
   expect_equal(combine_samples_n(mcmcr_example, mcmcr_example, mcmcr_example), mcmcr_example, check.attributes = FALSE)
   x <- combine_samples_n(mcmcr_example, mcmcr_example,  mcmcr_example, fun = sum)
   expect_equal(x$beta[1,,,], mcmcr_example$beta[1,,,] * 3)
