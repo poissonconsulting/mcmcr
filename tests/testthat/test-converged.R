e6be6da627fa15b3896f0da5f0eefe2a4dc37f40
joethorley <joe@poissonconsulting.ca>
2019-06-20 08:28:35 -0700

    reconfiguring converged tests

--- tests/testthat/test-converged.R
+++ tests/testthat/test-converged.R
@@ -1,7 +1,10 @@
 context("converged")
 
-test_that("converged", {
+test_that("converged.mcmcr", {
   expect_false(converged(mcmcr_example))
   expect_false(converged(mcmcr_example2))
+})
+
+test_that("converged.mcmcarray", {
   expect_true(converged(mcmcr_example$sigma))
 })
