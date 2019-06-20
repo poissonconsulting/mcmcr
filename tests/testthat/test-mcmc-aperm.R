e35c0f62bb3d196fbc620242ebef90cc798452a4
joethorley <joe@poissonconsulting.ca>
2019-06-20 09:14:10 -0700

    mcmc_aperm tests

--- tests/testthat/test-mcmc-aperm.R
+++ tests/testthat/test-mcmc-aperm.R
@@ -12,3 +12,21 @@ test_that("mcmc_aperm with big one", {
   expect_identical(pdims(mcmc_aperm(x)), rev(pdims(x)))
   expect_identical(pdims(mcmc_aperm(x, perm = c(2L, 4L))), c(5L, 7L, 4L, 6L))
 })
+
+test_that("mcmc_aperm.mcmc", {
+  expect_identical(mcmc_aperm(coda::as.mcmc(collapse_chains(mcmcr_example))),
+                   coda::as.mcmc(collapse_chains(mcmcr_example)))
+})
+
+test_that("mcmc_aperm.mcmc.list", {
+  expect_identical(mcmc_aperm(coda::as.mcmc.list(mcmcr_example)),
+                   coda::as.mcmc.list(mcmcr_example))
+})
+
+test_that("mcmc_aperm.mcmcr", {
+  expect_identical(mcmc_aperm(mcmcr_example), mcmcr_example)
+})
+
+test_that("mcmc_aperm.mcmcrs", {
+  expect_identical(mcmc_aperm(as.mcmcrs(list(mcmcr_example, mcmcr_example)), perm = 1L), as.mcmcrs(list(mcmcr_example, mcmcr_example)))
+})
