5eb791c49484351d8be6245a643ed15f17d5ae0a
joethorley <joe@poissonconsulting.ca>
2019-06-20 07:40:55 -0700

    test-as

--- tests/testthat/test-as.R
+++ tests/testthat/test-as.R
@@ -6,10 +6,21 @@ test_that("as.mcarray", {
   expect_equal(mcmcarrays, unclass(mcmcr_example), check.attributes = FALSE)
   mcarrays2 <- lapply(mcmcarrays, as.mcarray)
   expect_equal(mcarrays2, mcarrays)
+  expect_equal(mcarrays[[1]], as.mcarray(mcarrays[[1]]))
+
+  expect_error(as.mcarray(coda::as.mcmc.list(mcmcr_example)),
+               "x must only have 1 parameter")
+
+  expect_is(as.mcarray(coda::as.mcmc.list(mcmcr_example[[1]])), "mcarray")
+})
+
+test_that("as.mcmc", {
+  expect_error(coda::as.mcmc(mcmcr_example$alpha),
+               "x must only have 1 chain")
 })
 
 test_that("as.mcmc.list", {
-  mcmc.list <- as.mcmc.list(mcmcr_example)
+  mcmc.list <- coda::as.mcmc.list(mcmcr_example)
   expect_is(mcmc.list, "mcmc.list")
 
   expect_identical(coda::nchain(mcmc.list), 2L)
@@ -19,4 +30,12 @@ test_that("as.mcmc.list", {
 
 test_that("as.mcmcrs", {
   expect_is(as.mcmcrs(list(mcmcr_example, mcmcr_example)), "mcmcrs")
+})
+
+test_that("as.term", {
+  term <- as.term(coda::as.mcmc.list(mcmcr_example))
+  expect_identical(term,
+                   structure(c("alpha[1]", "alpha[2]", "beta[1,1]", "beta[2,1]",
+                               "beta[1,2]", "beta[2,2]", "sigma"), class = c("term", "character"
+                               )))
 })
