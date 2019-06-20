b12758575e092b8906a9aa5cc89a228a86d4dd18
joethorley <joe@poissonconsulting.ca>
2019-06-20 10:05:54 -0700

    more tests

--- tests/testthat/test-as.R
+++ tests/testthat/test-as.R
@@ -40,6 +40,8 @@ test_that("as.mcmc.list", {
 test_that("as.mcmcr", {
   expect_identical(as.mcmcr(unclass(mcmcr_example)), mcmcr_example)
   expect_identical(as.mcmcr(mcmcr_example), mcmcr_example)
+  expect_is(as.mcmcr(as.mcarray(mcmcr_example$beta)), "mcmcr")
+  expect_is(as.mcmcr(mcmcr_example$beta), "mcmcr")
 })
 
 test_that("as.mcmcrs", {
--- tests/testthat/test-bind-dimensions.R
+++ tests/testthat/test-bind-dimensions.R
@@ -5,12 +5,10 @@ test_that("bind_dimensions", {
   expect_identical(pdims(bind_dimensions(mcmcr_example, mcmcr_example)),
                    list(alpha = c(2L, 2L), beta = c(2L, 2L, 2L), sigma = c(1L, 2L)))
 
+  expect_identical(pdims(bind_dimensions(mcmcr_example, mcmcr_example, along = 1L)),
+                   list(alpha = c(4L), beta = c(4L, 2L), sigma = c(2L)))
+
   expect_identical(pdims(bind_dimensions(mcmcr_example2, mcmcr_example2)),
                    list(alpha = c(2L, 1L, 1L, 2L), beta = c(2L, 2L, 2L, 2L), sigma = c(1L, 1L, 1L, 2L)))
 
-})
-
-test_that("bind_dimensions_n", {
-  expect_identical(pdims(bind_dimensions_n(mcmcr_example, mcmcr_example, mcmcr_example)),
-                   list(alpha = c(2L, 3L), beta = c(2L, 2L, 3L), sigma = c(1L, 3L)))
 })
