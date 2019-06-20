b3078418be23613932bac96f84d3cda0e46d40f6
joethorley <joe@poissonconsulting.ca>
2019-06-20 08:11:21 -0700

    test plot

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -5,3 +5,9 @@ test_that("plot.mcmcarray", {
   expect_null(plot(mcmcr_example[[1]]))
   dev.off()
 })
+
+test_that("plot.mcmcarray", {
+  pdf(tempfile())
+  expect_null(plot(mcmcr_example))
+  dev.off()
+})
