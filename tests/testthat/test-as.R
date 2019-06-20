abc9ace46b09eecab92da7fb98abd66a1f0c25f2
joethorley <joe@poissonconsulting.ca>
2019-06-20 09:21:21 -0700

    test as.mcmc.list

--- tests/testthat/test-as.R
+++ tests/testthat/test-as.R
@@ -25,11 +25,15 @@ test_that("as.mcmc", {
 })
 
 test_that("as.mcmc.list", {
+  expect_is(coda::as.mcmc.list(as.mcmc(collapse_chains(mcmcr_example))),
+            "mcmc.list")
+
   mcmc.list <- coda::as.mcmc.list(mcmcr_example)
   expect_is(mcmc.list, "mcmc.list")
 
   expect_identical(coda::nchain(mcmc.list), 2L)
   expect_identical(coda::nvar(mcmc.list), 7L)
+  expect_identical(coda::as.mcmc.list(mcmc.list), mcmc.list)
   expect_equal(as.mcmcr(mcmc.list), mcmcr_example, check.attributes = FALSE)
 })
 
