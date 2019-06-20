3980aff5df6d301614b54cbc3c289b0ff700b48d
joethorley <joe@poissonconsulting.ca>
2019-06-20 08:17:47 -0700

    more test as

--- tests/testthat/test-as.R
+++ tests/testthat/test-as.R
@@ -12,11 +12,16 @@ test_that("as.mcarray", {
                "x must only have 1 parameter")
 
   expect_is(as.mcarray(coda::as.mcmc.list(mcmcr_example[[1]])), "mcarray")
+  expect_is(as.mcarray(as.mcarray(coda::as.mcmc.list(mcmcr_example[[1]]))), "mcarray")
 })
 
 test_that("as.mcmc", {
   expect_error(coda::as.mcmc(mcmcr_example$alpha),
                "x must only have 1 chain")
+
+
+  expect_is(as.mcmc(as.mcarray(coda::as.mcmc.list(collapse_chains(mcmcr_example[[1]])))),
+            "mcmc")
 })
 
 test_that("as.mcmc.list", {
@@ -28,8 +33,16 @@ test_that("as.mcmc.list", {
   expect_equal(as.mcmcr(mcmc.list), mcmcr_example, check.attributes = FALSE)
 })
 
+test_that("as.mcmcr", {
+  expect_identical(as.mcmcr(as.list(mcmcr_example)), mcmcr_example)
+  expect_identical(as.mcmcr(mcmcr_example), mcmcr_example)
+})
+
 test_that("as.mcmcrs", {
   expect_is(as.mcmcrs(list(mcmcr_example, mcmcr_example)), "mcmcrs")
+  expect_identical(as.mcmcrs(as.mcmcrs(list(mcmcr_example, mcmcr_example))),
+                   as.mcmcrs(list(mcmcr_example, mcmcr_example)))
+
 })
 
 test_that("as.term", {
