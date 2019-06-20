beac914d2542ac3ae64bd679322852f96da69e47
joethorley <joe@poissonconsulting.ca>
2019-06-20 09:08:33 -0700

    test converged

--- tests/testthat/test-converged.R
+++ tests/testthat/test-converged.R
@@ -8,3 +8,10 @@ test_that("converged.mcmcr", {
 test_that("converged.mcmcarray", {
   expect_true(converged(mcmcr_example$sigma))
 })
+
+test_that("converged.mcmcrs", {
+  expect_identical(converged(as.mcmcrs(list(mcmcr_example, mcmcr_example))),
+                   list(mcmcr1 = FALSE, mcmcr2 = FALSE))
+#  expect_identical(converged(as.mcmcrs(list(mcmcr_example, mcmcr_example)), as_df = TRUE),
+#                   list(mcmcr1 = FALSE, mcmcr2 = FALSE))
+})
