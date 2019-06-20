97943c9b7235ea8fad9af67bc8a82147850a11dc
joethorley <joe@poissonconsulting.ca>
2019-06-20 08:04:02 -0700

    test print

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -3,3 +3,11 @@ context("print")
 test_that("print.mcmcarray", {
   expect_output(print(mcmcr_example[[1]]))
 })
+
+test_that("print.mcmcr", {
+  expect_output(print(mcmcr_example))
+})
+
+test_that("print.mcmcr", {
+  expect_output(print(as.term(mcmcr_example)))
+})
