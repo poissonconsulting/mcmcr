14107462a80c06b14d80c368ac6e14674d504168
joethorley <joe@poissonconsulting.ca>
2019-06-30 18:58:39 -0700

    tdims in term

--- tests/testthat/test-is.R
+++ tests/testthat/test-is.R
@@ -1,7 +1,6 @@
 context("is")
 
 test_that("is", {
-  expect_true(is.term(term(mcmcr_example)))
   expect_true(is.mcmcarray(mcmcr_example[[1]]))
   expect_true(is.mcmcr(mcmcr_example))
   expect_true(is.mcmcrs(mcmcrs(mcmcr_example)))
