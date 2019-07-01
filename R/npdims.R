fa2bc3c2547a96259f9f5578dea0ae8d119c78e9
joethorley <joe@poissonconsulting.ca>
2019-06-30 19:23:44 -0700

    npdims

--- R/npdims.R
+++ R/npdims.R
@@ -1,31 +1,5 @@
-#' Number of Parameter Dimensions
-#'
-#' Gets the number parameter dimensions of an object.
-#'
-#' @param x The object
-#' @param ... Unused.
 #' @export
-#' @examples
-#' pdims(mcmcr_example)
-npdims <- function(x, ...) UseMethod("npdims")
+npdims.mcmc.list <- function(x, ...) lapply(pdims(x), length)
 
-#' @describeIn pdims Parameter dimensions for a term vector
 #' @export
-npdims.default <- function(x, ...) {
-  check_unused(...)
-  length(pdims(x))
-}
-
-#' @describeIn pdims Parameter dimensions for an mcmcr object
-#' @export
-npdims.mcmc.list <- function(x, ...) {
-  check_unused(...)
-  lapply(pdims(x), length)
-}
-
-#' @describeIn pdims Parameter dimensions for an mcmcr object
-#' @export
-npdims.mcmcr <- function(x, ...) {
-  check_unused(...)
-  vapply(x, npdims, 1L)
-}
+npdims.mcmcr <- function(x, ...) vapply(x, npdims, 1L)
--- R/pdims.R
+++ R/pdims.R
@@ -1,60 +1,14 @@
-#' Parameter Dimensions
-#'
-#' Gets the parameter dimensions of an object.
-#'
-#' @param x The object
-#' @param ... Unused.
 #' @export
-#' @examples
-#' pdims(mcmcr_example)
-pdims <- function(x, ...) UseMethod("pdims")
+pdims.mcarray <- function(x, ...) dims(x)[1:(length(dims(x))-2)]
 
-#' @describeIn pdims Parameter dimensions for a term vector
 #' @export
-pdims.term <- function(x, ...) {
-  check_unused(...)
-  x <- sort(x)
-  x <- split(x, pars(x, terms = TRUE))
-  x <- lapply(x, function(x) x[length(x)])
-  x <- unlist(x)
-  x <- sub("(^\\w+)(.*)", "\\2", as.character(x))
-  x <- sub("^$", "1", x)
-  x <- gsub("\\[|\\]", "", x)
-  x <- strsplit(x, ",")
-  lapply(x, as.integer)
-}
+pdims.mcmc <- function(x, ...) pdims(as.term(x))
 
-#' @describeIn pdims Parameter dimensions for an mcarray object
 #' @export
-pdims.mcarray <- function(x, ...) {
-  check_unused(...)
-  dims(x)[1:(length(dims(x))-2)]
-}
+pdims.mcmc.list <- function(x, ...) pdims(x[[1]])
 
-#' @describeIn pdims Parameter dimensions for an mcmc object
 #' @export
-pdims.mcmc <- function(x, ...) {
-  check_unused(...)
-  pdims(as.term(x))
-}
+pdims.mcmcarray <- function(x, ...) dims(x)[-(1:2)]
 
-#' @describeIn pdims Parameter dimensions for an mcmc.list object
 #' @export
-pdims.mcmc.list <- function(x, ...) {
-  check_unused(...)
-  pdims(x[[1]])
-}
-
-#' @describeIn pdims Parameter dimensions for an mcmcarray object
-#' @export
-pdims.mcmcarray <- function(x, ...) {
-  check_unused(...)
-  dims(x)[-(1:2)]
-}
-
-#' @describeIn pdims Parameter dimensions for an mcmcr object
-#' @export
-pdims.mcmcr <- function(x, ...) {
-  check_unused(...)
-  lapply(x, pdims)
-}
+pdims.mcmcr <- function(x, ...) lapply(x, pdims)
