8d46257a02b54f6677fe03f1fcde320da4d4c709
joethorley <joe@poissonconsulting.ca>
2019-07-04 13:52:04 -0700

    nsims.default and tests

--- R/nsims.R
+++ R/nsims.R
@@ -1,17 +1,2 @@
 #' @export
-nsims.mcarray <- function(x) nchains(x) * niters(x)
-
-#' @export
-nsims.mcmc <- function(x) nchains(x) * niters(x)
-
-#' @export
-nsims.mcmc.list <- function(x) nchains(x) * niters(x)
-
-#' @export
-nsims.mcmcarray <- function(x) nchains(x) * niters(x)
-
-#' @export
-nsims.mcmcr <- function(x) nchains(x) * niters(x)
-
-#' @export
-nsims.mcmcrs <- function(x) nchains(x) * niters(x)
+nsims.default <- function(x, ...) nchains(x) * niters(x)
---
+++ tests/testthat/test-nsims.R
@@ -0,0 +1,9 @@
+context("nsims")
+
+test_that("nsims",{
+  expect_identical(nsims(mcmcr::mcmcr_example), 800L)
+  expect_identical(nsims(as.mcmc.list(mcmcr::mcmcr_example)), 800L)
+  expect_identical(nsims(as.mcmc(subset(mcmcr::mcmcr_example, chains = 1L))), 400L)
+  expect_identical(nsims(as.mcarray(mcmcr::mcmcr_example[[1]])), 800L)
+  expect_identical(nsims(mcmcrs(mcmcr::mcmcr_example, mcmcr::mcmcr_example)), 800L)
+})
