7d91ff6d685b9501e0afd639b93b5414cd58f667
joethorley <joe@poissonconsulting.ca>
2019-07-23 12:24:27 -0700

    fix npdims after change in term

--- R/npdims.R
+++ R/npdims.R
@@ -2,4 +2,7 @@
 npdims.mcmc.list <- function(x, ...) vapply(pdims(x), length, 1L)
 
 #' @export
+npdims.mcmcarray <- function(x, ...) length(pdims(x))
+
+#' @export
 npdims.mcmcr <- function(x, ...) vapply(x, npdims, 1L)
