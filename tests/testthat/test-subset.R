fe04a7cef647d04c2b8b8fc534743c1cd3381e2a
joethorley <joe@poissonconsulting.ca>
2019-07-30 09:33:06 -0700

    parameters to pars

--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -1,7 +1,7 @@
 context("subset")
 
 test_that("subset.mcmcr", {
-  expect_identical(pars(subset(mcmcr_example, parameters = rev(pars(mcmcr_example)))), rev(parameters(mcmcr_example)))
+  expect_identical(pars(subset(mcmcr_example, parameters = rev(pars(mcmcr_example)))), rev(pars(mcmcr_example)))
   expect_identical(nchains(subset(mcmcr_example, 1L)), 1L)
   expect_identical(nsims(subset(mcmcr_example, rep(1L, 5), 2:3)), 10L)
   expect_identical(nterms(subset(mcmcr_example, parameters = "beta")), 4L)
@@ -9,7 +9,7 @@ test_that("subset.mcmcr", {
 
 test_that("subset.mcmcrs", {
   mcmcrs <- mcmcrs(mcmcr::mcmcr_example, mcmcr::mcmcr_example)
-  expect_identical(pars(subset(mcmcrs, parameters = rev(pars(mcmcrs)))), rev(parameters(mcmcrs)))
+  expect_identical(pars(subset(mcmcrs, parameters = rev(pars(mcmcrs)))), rev(pars(mcmcrs)))
   expect_identical(nchains(subset(mcmcrs, 1L)), 1L)
   expect_identical(nsims(subset(mcmcrs, rep(1L, 5), 2:3)), 10L)
   expect_identical(nterms(subset(mcmcrs, parameters = "beta")), 4L)
