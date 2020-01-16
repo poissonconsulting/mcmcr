11455f0cf6346f4d8c9a794aeeba3b07d2a6dfa1
joethorley <joe@poissonconsulting.ca>
2020-01-16 10:14:45 -0800

    chk_s3_class

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -2,7 +2,8 @@ context("checkr")
 
 test_that("check_mcmcarray",{
   expect_identical(check_mcmcarray(mcmcr::mcmcr_example$sigma), mcmcr::mcmcr_example$sigma)
-  expect_error(check_mcmcarray(1), "1 must inherit from class mcmcarray")
+  expect_error(check_mcmcarray(1), "^1 must inherit from S3 class 'mcmcarray'[.]$",
+               class = "chk_error")
 
   x <- set_class(1, "mcmcarray")
   expect_error(check_mcmcarray(x), "x must be an array")
@@ -15,11 +16,13 @@ test_that("check_mcmcarray",{
 test_that("check_mcmcr",{
   expect_identical(check_mcmcr(mcmcr::mcmcr_example), mcmcr::mcmcr_example)
   expect_identical(check_mcmcr(mcmcr::mcmcr_example, sorted = TRUE), mcmcr::mcmcr_example)
-  expect_error(check_mcmcr(1), "1 must inherit from class mcmcr")
+  expect_error(check_mcmcr(1), "^1 must inherit from S3 class 'mcmcr'[.]$",
+               class = "chk_error")
 
   y <- set_class(list(x = 1), "mcmcr")
   expect_error(check_mcmcr(y),
-               "parameter 'x' of y must inherit from class mcmcarray")
+               "^Parameter 'x' of y must inherit from S3 class 'mcmcarray'[.]$",
+               class = "chk_error")
 
   mcmcr <- mcmcr::mcmcr_example
   is.na(mcmcr$sigma[2,2,1]) <- TRUE
