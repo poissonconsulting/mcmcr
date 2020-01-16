e3279aeb451830c300566cfde9b1edb6ff2b9437
joethorley <joe@poissonconsulting.ca>
2020-01-16 09:48:50 -0800

    chk_whole_number

--- R/namespace.R
+++ R/namespace.R
@@ -1,5 +1,5 @@
 #' @import err checkr term nlist
-#' @importFrom chk chk_unused chk_null chkor chk_flag chk_named chk_unique chk_string chk_function chk_sorted
+#' @importFrom chk chk_unused chk_null chkor chk_flag chk_named chk_unique chk_string chk_function chk_sorted chk_whole_number
 #' @importFrom coda as.mcmc.list as.mcmc thin mcmc.list
 #' @importFrom graphics plot
 #' @importFrom stats coef terms
