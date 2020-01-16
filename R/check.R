aa169e8d980c150f0d6d2574e9a904b2d2301033
joethorley <joe@poissonconsulting.ca>
2020-01-16 09:43:34 -0800

    chk_sorted

--- R/check.R
+++ R/check.R
@@ -41,7 +41,7 @@ check_mcmcr <- function(x, sorted = FALSE, x_name = substitute(x), error = TRUE)
   check_inherits(x, "mcmcr", x_name = x_name)
   chk_named(x)
   chk_unique(names(x))
-  if(sorted) check_sorted(names(x), x_name = p0("names of ", x_name), error = error)
+  if(sorted) chk_sorted(names(x), x_name = p0("names of ", x_name))
   mapply(check_mcmcarray, x, x_name = p0("parameter '", pars(x), "' of ", x_name),
          MoreArgs = list(error = error))
   invisible(x)
