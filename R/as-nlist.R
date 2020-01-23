8a15088dc7b4143cb3b7a1b0166ecefa25c52155
joethorley <joe@poissonconsulting.ca>
2020-01-23 15:56:54 -0800

    as.nlist to nlist

--- R/as-nlist.R
+++ R/as-nlist.R
@@ -1,13 +1,19 @@
 #' @export
+nlist::as.nlist
+
+#' @inherit nlist::as.nlist
+#' @export
 as.nlist.mcmc <- function(x, ...) {
   as.nlist(as.mcmcr(x))
 }
 
+#' @inherit nlist::as.nlist
 #' @export
 as.nlist.mcmc.list <- function(x, ...) {
   as.nlist(as.mcmcr(x))
 }
 
+#' @inherit nlist::as.nlist
 #' @export
 as.nlist.mcmcr <- function(x, ...) {
   chk_identical(nchains(x), 1L)
