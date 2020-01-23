a73dec563e86598c65bf8489870a8cf70de0a7aa
joethorley <joe@poissonconsulting.ca>
2020-01-23 15:59:01 -0800

    nchains to universals

--- R/nchains.R
+++ R/nchains.R
@@ -1,38 +1,28 @@
-#' Number of MCMC chains
-#'
-#' Gets the number of chains of an MCMC object.
-#'
-#' @param x An MCMC object
-#' @param ... Unused
-#' @return A count indicating the number of MCMC chains
 #' @export
-#' @name nchains
-#' @examples
-#' nchains(mcmcr::mcmcr_example)
-NULL
+universals::niters
 
 nchains.matrix <- function(x, ...) nrow(x)
 
-#' @describeIn nchains Number of chains of an mcarray object
+#' @inherit universals::nchains
 #' @export
 nchains.mcarray <- function(x, ...) unname(dim(x)[ndims(x)])
 
-#' @describeIn nchains Number of chains of an mcmc object
+#' @inherit universals::nchains
 #' @export
 nchains.mcmc <- function(x, ...) 1L
 
-#' @describeIn nchains Number of chains of an mcmc.list object
+#' @inherit universals::nchains
 #' @export
 nchains.mcmc.list <- function(x, ...) length(x)
 
-#' @describeIn nchains Number of chains of an mcmcarray object
+#' @inherit universals::nchains
 #' @export
 nchains.mcmcarray <- function(x, ...) dim(x)[1]
 
-#' @describeIn nchains Number of chains of an mcmcr object
+#' @inherit universals::nchains
 #' @export
 nchains.mcmcr <- function(x, ...) nchains(x[[1]])
 
-#' @describeIn nchains Number of chains of an mcmcrs object
+#' @inherit universals::nchains
 #' @export
 nchains.mcmcrs <- function(x, ...) nchains(x[[1]])
