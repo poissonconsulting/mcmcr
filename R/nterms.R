de248e8c794c9a3fa951116a652b4c67c9a5ea6b
joethorley <joe@poissonconsulting.ca>
2020-01-23 15:45:23 -0800

    nterms universals

--- R/nterms.R
+++ R/nterms.R
@@ -1,14 +1,22 @@
 #' @export
+universals::nterms
+
+#' @inherit universals::nterms
+#' @export
 nterms.mcmc <- function(x, ...) ncol(x)
 
+#' @inherit universals::nterms
 #' @export
 nterms.mcmc.list <- function(x, ...) nterms(x[[1]])
 
+#' @inherit universals::nterms
 #' @export
 nterms.mcmcarray <- function(x, ...) as.integer(prod(dim(x)[-(1:2)]))
 
+#' @inherit universals::nterms
 #' @export
 nterms.mcmcr <- function(x, ...) sum(vapply(x, nterms, 1L))
 
+#' @inherit universals::nterms
 #' @export
 nterms.mcmcrs <- function(x, ...) nterms(x[[1]])
