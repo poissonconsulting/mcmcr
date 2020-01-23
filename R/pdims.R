572f99e302fe18edb7c589a5a4b353ca5e9a07b3
joethorley <joe@poissonconsulting.ca>
2020-01-23 15:50:07 -0800

    pdims to universals

--- R/pdims.R
+++ R/pdims.R
@@ -1,14 +1,22 @@
 #' @export
+universals::pdims
+
+#' @inherit universals::pdims
+#' @export
 pdims.mcarray <- function(x, ...) dims(x)[1:(length(dims(x))-2)]
 
+#' @inherit universals::pdims
 #' @export
 pdims.mcmc <- function(x, ...) pdims(as.term(x))
 
+#' @inherit universals::pdims
 #' @export
 pdims.mcmc.list <- function(x, ...) pdims(x[[1]])
 
+#' @inherit universals::pdims
 #' @export
 pdims.mcmcarray <- function(x, ...) dims(x)[-(1:2)]
 
+#' @inherit universals::pdims
 #' @export
 pdims.mcmcr <- function(x, ...) lapply(x, pdims)
