7ba07b587679b1a47c3f3bed5506a4a0336e7d1e
joethorley <joe@poissonconsulting.ca>
2020-01-23 16:11:29 -0800

    npars to universals

--- R/npars.R
+++ R/npars.R
@@ -1,17 +1,26 @@
 #' @export
+universals::npars
+
+#' @inherit universals::npars
+#' @export
 npars.mcmc <- function(x, ...) npars(as.term(x))
 
+#' @inherit universals::npars
 #' @export
 npars.mcmc.list <- function(x, ...) npars(x[[1]])
 
+#' @inherit universals::npars
 #' @export
 npars.mcarray <- function(x, ...) 1L
 
+#' @inherit universals::npars
 #' @export
 npars.mcmcarray <- function(x, ...) 1L
 
+#' @inherit universals::npars
 #' @export
 npars.mcmcr <- function(x, ...) length(x)
 
+#' @inherit universals::npars
 #' @export
 npars.mcmcrs <- function(x, ...) npars(x[[1]])
