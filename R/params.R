17dc436cffac1ffa30eaec67ca150d17d48c0b99
joethorley <joe@poissonconsulting.ca>
2020-01-23 16:39:57 -0800

    pars to universals

---
+++ R/params.R
@@ -0,0 +1,8 @@
+#' Parameter Descriptions
+#'
+#' @param x An object.
+#' @param scalar A logical scalar specifying whether to only include scalar terms.
+#' @param terms A logical scalar specifying whether to provide the parameters for each term.
+#'
+#' @name params
+NULL
--- R/pars.R
+++ R/pars.R
@@ -1,15 +1,24 @@
 #' @export
+universals::pars
+
+#' @inherit universals::pars
+#' @inheritParams params
+#' @export
 pars.mcmc <- function(x, scalar = NA, terms = FALSE, ...) {
   chk_unused(...)
   pars(as.term(x), scalar = scalar, terms = terms)
 }
 
+#' @inherit universals::pars
+#' @inheritParams params
 #' @export
 pars.mcmc.list <- function(x, scalar = NA, terms = FALSE, ...) {
   chk_unused(...)
   pars(x[[1]], scalar = scalar, terms = terms)
 }
 
+#' @inherit universals::pars
+#' @inheritParams params
 #' @export
 pars.mcmcr <- function(x, scalar = NA, terms = FALSE, ...) {
   chk_unused(...)
@@ -17,6 +26,8 @@ pars.mcmcr <- function(x, scalar = NA, terms = FALSE, ...) {
   pars(as.term(x), scalar = scalar, terms = terms)
 }
 
+#' @inherit universals::pars
+#' @inheritParams params
 #' @export
 pars.mcmcrs <- function(x, scalar = NA, terms = FALSE, ...) {
   chk_unused(...)
