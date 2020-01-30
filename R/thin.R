1632094515c1e6b1517b629ab7fe86ce49c5ed92
joethorley <joe@poissonconsulting.ca>
2020-01-30 01:16:21 -0800

    moved thin to nlist

--- R/thin.R
+++ R/thin.R
@@ -1,18 +1,2 @@
 #' @export
 coda::thin
-
-#' Thin MCMC Object
-#'
-#' Thins an MCMC object's iterations.
-#'
-#' @inheritParams params
-#' @return The thinned MCMC object.
-#' @export
-#' @examples
-#' thin(mcmcr_example, nthin = 10L)
-thin.default <- function(x, nthin = 1L,...) {
-  chk_whole_number(nthin)
-  chk_gte(nthin, 1)
-  iters <- seq(1L, niters(x), by = nthin)
-  subset(x, iters = iters)
-}
