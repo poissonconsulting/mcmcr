ea89a02009685df8952f84491b61f38842275f00
Kirill Müller <krlmlr+r@mailbox.org>
2021-05-10 20:13:18 +0200

    WIP: Restore original implementation of as_nlists.mcmcr()
    This fixes the performance problem by reverting to the original implementation. For the simple example the results didn't change. I don't understand the implications of this.
    
    Closes #47.

--- R/as-nlists.R
+++ R/as-nlists.R
@@ -4,5 +4,8 @@ nlist::as_nlists
 #' @inherit nlist::as_nlists
 #' @export
 as_nlists.mcmcr <- function(x, ...) {
-  as_nlists(as_mcmc_list(x))
+  chk_identical(nchains(x), 1L)
+  x <- lapply(1:niters(x), FUN = subset_iteration_mcmcr, x)
+  x <- lapply(x, set_class, "nlist")
+  set_class(x, "nlists")
 }
