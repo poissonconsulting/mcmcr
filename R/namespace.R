a4c831d69b547104de833f076d23ce4faa028e13
Joe Thorley <joe@poissonconsulting.ca>
2021-08-05 09:27:48 -0700

    make sure import one from generics

--- R/namespace.R
+++ R/namespace.R
@@ -1,5 +1,6 @@
 #' @import chk universals term nlist extras
 #' @importFrom coda as.mcmc.list as.mcmc thin mcmc.list
+#' @importFrom generics tidy
 #' @importFrom graphics plot
 #' @importFrom stats coef terms median na.pass
 NULL
