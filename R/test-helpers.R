05f0b8a0ef7715a1d9f8920f627bee06500d5c05
Joe Thorley <joe@poissonconsulting.ca>
2021-09-06 10:38:50 -0700

    actually skip_on_os

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -5,7 +5,7 @@ save_csv <- function(x) {
 }
 
 expect_snapshot_data <- function(x, name) {
-  skip_on("windows")
+  testthat::skip_on_os("windows")
   path <- save_csv(x)
   testthat::expect_snapshot_file(path, paste0(name, ".csv"))
 }
