3e90e0215b34e47ff3d0e7e5652dbd54f5904f82
Joe Thorley <joe@poissonconsulting.ca>
2021-09-06 10:34:25 -0700

    skip snapshot on windows

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -5,6 +5,7 @@ save_csv <- function(x) {
 }
 
 expect_snapshot_data <- function(x, name) {
+  skip_on("windows")
   path <- save_csv(x)
   testthat::expect_snapshot_file(path, paste0(name, ".csv"))
 }
