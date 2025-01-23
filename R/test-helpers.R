ea5f16e79bc0edfc1ac16af5f57981c93fbdb4a5
Joe Thorley <joe@poissonconsulting.ca>
2025-01-23 07:07:20 -0800

    actually remove |>

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -6,7 +6,7 @@ save_csv <- function(x) {
 
 expect_snapshot_data <- function(x, name, digits = 6) {
   fun <- function(x) signif(x, digits = digits)
-  x <- as.list(x) |>
+  x <- as.list(x)
   x <- purrr::map_if(x, .p = is.numeric, .f = signif, digits = digits)
   x <- as.data.frame(x)
   path <- save_csv(x)
